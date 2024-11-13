51182c6951948a6cde01d988d181d878da46591a
ProjectSoft <projectsoft2009@yandex.ru>
2024-11-14 00:28:31 +0400

    update

--- site/assets/tvs/multitv/configs/share.config.inc.php
+++ site/assets/tvs/multitv/configs/share.config.inc.php
@@ -12,10 +12,7 @@ $settings['fields'] = array(
 	)
 );
 $settings['templates'] = array(
-	'outerTpl' => '<div class="footer-sendbtn">
-		<button class="callme-btn btn" data-trigger="sendbot">ФОРМА ОБРАТНОЙ СВЯЗИ</button>
-	</div>
-	<div class="footer-icons"><div class="icons"><ul class="footer-icons-menu"><li><a class="icon-share" href="[!getFullUrl!]#" down-link="share" title="Поделиться"></a><ul class="submenu">[+wrapper+]</ul></li></ul></div></div>',
+	'outerTpl' => '<div class="footer-icons"><div class="icons"><ul class="footer-icons-menu"><li><a class="icon-share" href="[!getFullUrl!]#" down-link="share" title="Поделиться"></a><ul class="submenu">[+wrapper+]</ul></li></ul></div></div>',
 	'rowTpl' => '<li><a class="icon-[+type+]" href="[!getFullUrl!]#" down-link="[+type+]" title="[+title+]"></a></li>'
 );
 $settings['configuration'] = array(
