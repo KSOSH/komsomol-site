896e9ce20190baccdef02ba8ca4ca283c675770f
ProjectSoft <projectsoft2009@yandex.ru>
2025-01-17 11:16:42 +0400

    update

--- site/assets/plugins/dashboard_widgets/help_widget/help_widget.php
+++ site/assets/plugins/dashboard_widgets/help_widget/help_widget.php
@@ -1,17 +1,28 @@
-<?php 
+<?php
+if (!defined('MODX_BASE_PATH')) {
+	http_response_code(403);
+	die('For'); 
+}
 $e = &$modx->Event;
 $help_content = '';
+$ttl = "";
 $chelp = "";
 $id_help = isset($id_help) ? intval($id_help) : 1;
 switch($e->name){
-	case 'OnManagerWelcomeHome': 
+	case 'OnManagerWelcomeHome':
+		$base = str_replace('\\', '/', dirname(__FILE__)) . '/';
+		$url = $modx->config["site_url"] . str_replace(MODX_BASE_PATH, '', $base);
 		$tbl_site_content = $modx->getFullTableName('site_content');
 		$rs = $modx->db->select('content', $tbl_site_content, "id='{$id_help}'");
 		$chelp = $modx->db->getValue($rs);
+		$rs = $modx->db->select('pagetitle', $tbl_site_content, "id='{$id_help}'");
+		$ttl = $modx->db->getValue($rs);
 		$help_content .=  '
 <div id="help_content">
 	<div id="frame_help">
 		' . $chelp . '
+		<div data-plugin="emoji"></div>
+		<script src="' . $url . 'js/emoji.min.js?' . filemtime($base . 'js/emoji.min.js') . '#hr=true&title=true&content=true"></script>
 	</div>
 </div>';
 		$menuindex = isset($menuindex) ? (int)$menuindex : 10;
@@ -20,16 +31,8 @@ switch($e->name){
 			'id' => 'help-widget',
 			'cols' => 'col-sm-12',
 			'icon' => 'fa-question-circle',
-			'title' => 'Помощь',
-			'body' => '<div class="card-body">'.$help_content.'</div>
-			<style>
-				.list-decimal > li {
-					list-style: decimal;
-				}
-				h3, .h3 {
-					color: red;
-				}
-			</style>'
+			'title' => $ttl,
+			'body' => '<div class="card-body">' . $help_content.'</div>'
 		);
 		$modx->event->output(serialize($widgets));
 		break;
